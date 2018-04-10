4a72bcfe9c1b7528e0dc5db9a7d1a12615147e0a
Chris Crowder <chris.crowder.95@gmail.com>
2018-04-10 17:18:24 -0400

    added player left/right controls and the random spawning of shapes

--- tetris.js
+++ tetris.js
@@ -1,4 +1,4 @@
-window.onload = function () {
+window.onload = function() {
 
 	const canvas = document.getElementById('play_area');
 	const ctx = canvas.getContext('2d');
@@ -6,6 +6,7 @@ window.onload = function () {
 	const gameRowCount = 24;
 	const gameColumnCount = 10;
 	const squareSize = 30;
+	const startPosition = {row: 0, col: 3};
 
 	canvas.width = gameColumnCount * squareSize;
 	canvas.height = (gameRowCount) * squareSize;
@@ -13,16 +14,35 @@ window.onload = function () {
 	ctx.fillStyle = 'black';
 
 
-	// creates a 2d array with the given dimmensions
-	function createMatrix (w, h) {
-	    let matrix = [];
-	    while (h--) {
-	    	matrix.push(new Array(w).fill(0));
-	    }
-	    return matrix;
+	// returns a random integer within 0 through max
+	function randomInt(max) {
+		return Math.floor(Math.random() * Math.floor(max));
 	}
 
 
+	// creates a 2d array with the given dimmensions
+	function createMatrix(w, h) {
+		let matrix = [];
+		while (h--) {
+			matrix.push(new Array(w).fill(0));
+		}
+		return matrix;
+	}
+
+
+	// returns a random shape
+	function randomShape() {
+		let count = 0;
+		let choice = randomInt(7);
+		for (let shape in shapes) {
+			if (count++ == choice) {
+				return shapes[shape];
+			}
+		}
+	}
+
+
+	// this tracks all of the pieces that have been set and are not controlled by the player
 	class FrozenPieces {
 		constructor() {
 			this.matrix = createMatrix(gameColumnCount, gameRowCount);
@@ -41,15 +61,28 @@ window.onload = function () {
 				}
 			}
 		}
+
+		addPiece(piece) {
+			for (let row = 0; row < piece.shape.length; row++) {
+				for (let col = 0; col < piece.shape[row].length; col++) {
+					if (piece.shape[row][col] != 0) {
+						this.matrix[row + piece.position.row][col + piece.position.col] = piece.shape[row][col];
+					}
+				 }
+			}
+		}
+
 	}
 	
 
+	// this class is used to track the falling piece that is controlled by the player
 	class FallingPiece {
-		constructor(shape) {
-			this.shape = shape;
+		constructor() {
+			let temp = randomShape();
+			this.color = temp.color;
+			this.shape = temp.variants[randomInt(temp.variants.length)];
 			this.position = { row: 0, col: 3 };
 		}
-
 
 		draw() {
 			for (let row = 0; row < this.shape.length; row++) {
@@ -63,7 +96,7 @@ window.onload = function () {
 			}
 		}
 
-
+		// moves the piece down by 1 row
 		shiftDown(board) {
 
 			let potentialPosition = {row: this.position.row + 1, col: this.position.col};
@@ -91,17 +124,52 @@ window.onload = function () {
 
 			// piece did not have any issues, update to new position
 			if (safe) {
-				console.log('position updated');
+				// console.log('position updated');
+				this.position = potentialPosition
+			
+			// piece could not be moved any more, add to board
+			} else {
+				board.addPiece(this);
+				this.shape = randomShape().variants[0];
+				this.position = startPosition;
+			}
+		}
+
+		// moves the piece left or right depending on given input
+		shiftAcross(direction) {
+
+			let potentialPosition = {row: this.position.row, col: this.position.col + direction};
+			let safe = true;
+
+			for (let row = 0; row < this.shape.length; row++) {
+				for (let col = 0; col < this.shape[row].length; col++) {
+					if (this.shape[row][col] != 0) {
+
+						let checkRow = row + potentialPosition.row;
+						let checkCol = col + potentialPosition.col;
+
+						// the piece has reached the bottom of the board
+						if (col >= board.matrix[0].length) {
+							safe = false;
+
+						// the piece has collided with a set block
+						} else if (board.matrix[checkRow][checkCol] !== 0) {
+							safe = false;
+						}
+
+					}	
+				}
+			}
+
+			// piece did not have any issues, update to new position
+			if (safe) {
+				// console.log('position updated');
 				this.position = potentialPosition
 			
 			// piece could not be moved any more, add to board
 			} else {
 
 			}
-		}
-
-
-		shiftAcross(direction) {
 
 		}
 
@@ -113,19 +181,54 @@ window.onload = function () {
 
 	}
 
+	// create the board and player objects
 	let board = new FrozenPieces();
-	let player = new FallingPiece(shapes['L'].variants[0]);
+	let player = new FallingPiece();
+	
+	// draws the current state of the game
+	function update() {
+		ctx.clearRect(0, 0, canvas.width, canvas.height);
+		board.draw();
+		player.draw();
+	}
+
+	document.onkeydown = check;
+
+	// function to check for player keypresses
+	function check(e) {
+
+		e = e || window.event;
+
+		if (e.keyCode == '38') {
+			// up arrow
+			console.log('up key');
+		}
+
+		else if (e.keyCode == '40') {
+			// down arrow
+			player.shiftDown(board);
+			update();			
+		}
+
+		else if (e.keyCode == '37') {
+			// left arrow
+			player.shiftAcross(-1);
+			update();	   
+		}
+
+		else if (e.keyCode == '39') {
+			// right arrow
+			player.shiftAcross(1);
+			update();	   
+		}
+		
+	}
 
 	// game timer
 	let gameTimer = setInterval( function() {
 		
-		ctx.clearRect(0, 0, canvas.width, canvas.height);
-
-		board.draw();
-
-		player.draw();
+		update();
 		player.shiftDown(board);
-
 
 	}, 300);
 
